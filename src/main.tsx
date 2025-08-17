ba0fe5500c553d881b2d8c7d828a4b2d27437c7e
Nguyen Hao Nam <52100979@student.tdtu.edu.vn>
2025-08-17 19:31:10 +0700

    define relative import

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from '~/App.tsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
-import theme from './theme.ts'
+import theme from '~/theme.ts'
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
