cc1b41efa5d313cb3e68774565dcca0e354cdc52
Nguyen Hao Nam <52100979@student.tdtu.edu.vn>
2025-08-21 19:46:07 +0700

    Show Board Bar

--- src/pages/Boards/BoardBar/index.tsx
+++ src/pages/Boards/BoardBar/index.tsx
@@ -1,17 +1,95 @@
 import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import VPNLockIcon from "@mui/icons-material/VpnLock";
+import AddToDriveIcon from "@mui/icons-material/AddToDrive"
+import BoltIcon from "@mui/icons-material/Bolt"
+import FilterListIcon from "@mui/icons-material/FilterList"
+import AvatarGroup from "@mui/material/AvatarGroup";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import AvatarImg from "~/assets/img/Avatar.JPG";
+import Button from "@mui/material/Button";
+import PersonAddIcon from "@mui/icons-material/PersonAdd"
+
+const MENU_STYLES = {
+  color: "primary.main",
+  bgcolor: "white",
+  border: "none",
+  paddingX: "5px",
+  borderRadius: "4px",
+  "& .MuiSvgIcon-root": {
+    color: "primary.main",
+  },
+  "&:hover": {
+    bgcolor: "primary.50",
+  },
+};
 
 function BoardBar() {
   return (
     <Box
+      px={2}
       sx={{
-        backgroundColor: "primary.dark",
         width: "100%",
         height: (theme) => theme.trello.broadBarHeight,
         display: "flex",
         alignItems: "center",
+        justifyContent: "space-between",
+        gap: 2,
+        overflowX: "auto",
+        borderTop: "1px solid #00bfa5",
       }}
     >
-      Broad bar
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label="HaoNamDev MERN Stack Board"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VPNLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label="Filters"
+          clickable
+        />
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon />}>Invite</Button>
+        <AvatarGroup
+          max={4}
+          sx={{
+            "& .MuiAvatar-root": {
+              width: 34,
+              height: 34,
+              fontSize: 16,
+            },
+          }}
+        >
+          <Tooltip title="HaoNamDev">
+            <Avatar alt="HaoNamDev" src={AvatarImg} />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   );
 }
